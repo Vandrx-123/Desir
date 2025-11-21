4b8a10d5409b9b34a5f2d26ba138a21f023f5387
Desir Daxton Van Goebels <desirdaxtonvangoebels@gmail.com>
2025-11-21 16:49:42 -0500

    first commit

---
+++ app/src/main/java/com/example/desirproject/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.desirproject
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val questionText = findViewById<TextView>(R.id.Dax_question)
+        val answerText = findViewById<TextView>(R.id.Dax_Answer)
+
+        questionText.setOnClickListener {questionText.visibility = View.INVISIBLE}
+        answerText.setOnClickListener {answerText.visibility = View.VISIBLE}
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
